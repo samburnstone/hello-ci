d24485aabf3a599381fc10ba17232389f04433b4
Andrew Polkinghorn <apolkinghorn@scottlogic.co.uk>
2016-07-07 17:58:58 +0100

    Add single view project.

---
+++ ios/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Andrew Polkinghorn on 07/07/2016.
+//  Copyright © 2016 Andrew Polkinghorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
